d36acf255d49847f502b1890ef12185628c2b257
YanaKukushkina <yanikkuuu2017@gmail.com>
2026-07-13 18:51:51 +0300

    Update deploy.sh

--- infra/prod/infra/prod/deploy.sh
+++ infra/prod/infra/prod/deploy.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+# Запускаем систему
+docker-compose up -d
+
+# Проверяем здоровье сервисов
+docker-compose ps --services --filter=health=unhealthy | grep .
+if [ $? == 0 ]; then
+  echo "Health check failed! Похоже, откат при падении хелсчека никто не прописал..."
+fi
